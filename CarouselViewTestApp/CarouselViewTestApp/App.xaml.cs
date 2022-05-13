9e829a3e6f581b8dc11ecdb671b265c0c6824eb2
cbaer-extron <101732760+cbaer-extron@users.noreply.github.com>
2022-05-13 12:50:57 -0700

    CarouselViewError
    This error occurs when a CarouselView is within Shell.   Also, CarouselView does not work in Windows even if it is not within Shell.

---
+++ CarouselViewTestApp/CarouselViewTestApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CarouselViewTestApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell(); //  new MainPage();// 
+    }
+}
---
+++ CarouselViewTestApp/CarouselViewTestApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace CarouselViewTestApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+	List<string> testStringList;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		testStringList = new List<string>();
+		testStringList.Add("ONE");
+        testStringList.Add("TWO");
+        testStringList.Add("THREE");
+        testStringList.Add("FOUR");
+
+		carousel.ItemsSource = testStringList;	
+    }
+
+
+}
+
